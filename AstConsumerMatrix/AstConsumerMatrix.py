7445647d6f2926d8d6d5f5dd22be15a5d6c703df
ProTrack <harel@protrack.co.il>
2017-10-03 17:53:27 +0300

    got matrix output

--- AstConsumerMatrix/AstConsumerMatrix.py
+++ AstConsumerMatrix/AstConsumerMatrix.py
@@ -34,7 +34,6 @@ class FuncCallVisitor(ast.NodeVisitor):
 def get_func_calls(tree):
     class_calls = {}
     c = "main"
-    class_calls[c] = []
     for node in ast.walk(tree):
         if isinstance(node, ast.ClassDef):
             c = node.name
@@ -55,38 +54,41 @@ def get_func_calls(tree):
 def output(calls):
     import numpy as np
     from matplotlib import pyplot as plt
+    from matplotlib import gridspec as mtgs
+    from matplotlib.backends.backend_pdf import PdfPages
 
-    x = list(set(calls.keys))
-    y = list(set(calls.values))
-    Interfaces = np.zeros((len(y),len(x)), dtype=np.int)
+    x = list(set(calls.keys()))
+    y = list(set([item for sublist in calls.values() for item in sublist]))
+    #Interfaces = np.zeros((len(y),len(x)), dtype=np.int)
     Dependencies = np.zeros((len(y),len(x)), dtype=np.int)
-    for item,method in d[pattern].Interfaces:
-        Interfaces[pattern][y.index(method),x.index(item)] = 1
-    for method,item in list(set(d[pattern].Dependencies)):
-        if method in y and item in x:
-            Dependencies[pattern][y.index(method),x.index(item)] = 1
+    for item,funcCalls in calls.items():
+        for call in funcCalls:
+         Dependencies[y.index(call),x.index(item)] = 1
+    #for method,item in list(set(d[pattern].Dependencies)):
+    #    if method in y and item in x:
+    #        Dependencies[pattern][y.index(method),x.index(item)] = 1
 
     fig = plt.figure(figsize = (len(x),len(y)))
-    fig.suptitle(pattern, fontsize=14)           
-    gs = matplotlib.gridspec.GridSpec(nrows=2,ncols=1,right=0.98)
+    fig.suptitle('Consumer', fontsize=14)           
+    gs = mtgs.GridSpec(nrows=2,ncols=1,right=0.98)
 
-    vals = Interfaces[pattern]*-100+300
+    #vals = Interfaces*-100+300
 
-    ax1 = fig.add_subplot(gs[0])
-    ax1.axis('off')
-    ax1.set_title('Interfaces')
-    the_table = ax1.table(cellText=Interfaces[pattern], loc='center',cellLoc='center',rowLabels=y, colLabels=x,cellColours=plt.cm.hot(vals))
-    #[the_table._cells[(i,0)].set_width(0.1) for i in range(0,len(y))]
+    #ax1 = fig.add_subplot(gs[0])
+    #ax1.axis('off')
+    #ax1.set_title('Interfaces')
+    #the_table = ax1.table(cellText=Interfaces[pattern], loc='center',cellLoc='center',rowLabels=y, colLabels=x,cellColours=plt.cm.hot(vals))
+    ##[the_table._cells[(i,0)].set_width(0.1) for i in range(0,len(y))]
     
-    vals = Dependencies[pattern]*-100+300
+    vals = Dependencies*-100+300
     ax2 = fig.add_subplot(gs[1])
     ax2.axis('off')
     ax2.set_title('Dependencies')
-    the_table = ax2.table(cellText=Dependencies[pattern], loc='center',cellLoc='center',rowLabels=y, colLabels=x,cellColours=plt.cm.hot(vals))
+    the_table = ax2.table(cellText=Dependencies, loc='center',cellLoc='center',rowLabels=y, colLabels=x,cellColours=plt.cm.hot(vals))
     #the_table.scale(1.5, 1.5)
     #fig.subplots_adjust(left=0.3),colWidths=[0.15]*len(y),colWidths=[0.15]*len(y)
     #plt.show()
-    plt.savefig(pattern+'.pdf',bbox_inches='tight')
+    plt.savefig('Consumer.pdf',bbox_inches='tight')
 
     
 
